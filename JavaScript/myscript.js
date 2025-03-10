0093e3cd25cc112e342df72f4778c57d4cd72e8b
Kartik2007sharma <157768124+Kartik2007sharma@users.noreply.github.com>
2025-03-10 19:03:04 +0530

    Add files via upload

---
+++ JavaScript/myscript.js
@@ -0,0 +1,4 @@
+document.write("Hello World External");
+window.alert("Hello World External!");
+console.log("Hello World External !!");
+document.getElementById("para").innerHTML = "Hello World External !!!!";
